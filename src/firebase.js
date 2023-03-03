11c6aa39cc9ef1d117253f4f4b540310d3ba4211
Ankit <varshneyankit011@gmail.com>
2023-03-04 01:08:21 +0530

    The react context api is used to add the basket functionality and to manage some state variable like basket item and the firebase config is added.

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB63GG6Ws9x-u2I5-GeyYjmODXJgI9EyPA',
+  authDomain: 'clone-6a247.firebaseapp.com',
+  projectId: 'clone-6a247',
+  storageBucket: 'clone-6a247.appspot.com',
+  messagingSenderId: '628549815697',
+  appId: '1:628549815697:web:aaa2c1528c09bce2c95e30',
+  measurementId: 'G-6BHH16VRHG',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
